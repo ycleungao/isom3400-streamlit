7f5a8fa3146fa821360c09b45d02514beb9babb4
ycleungao <ycleungao@connect.ust.hk>
2025-11-17 14:44:53 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
 st.title("Retail Business Dashboard")
 
 st.header("Manage Input Section")
 
 st.write("Please enter the monthly sales target and select the region.")
-st.write("")
+target = st.number_input("Enter monthly sales target(in USD)",min_value = 0,max_value = 1000000,value = 50000)
+region = st.selectbox("Select Region",["North","East","South","West"])
+st.button("Submit")
+st.success(f"Input successfully! Your monthly sales target is {target} in {region} Region! ")
