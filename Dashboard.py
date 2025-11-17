32431aece5817427b133617d2efd6060c207b6c7
ycleungao <ycleungao@connect.ust.hk>
2025-11-17 14:39:33 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+st.title("Retail Business Dashboard")
+
+st.header("Manage Input Section")
+
+st.write("Please enter the monthly sales target and select the region.")
+st.write("")
