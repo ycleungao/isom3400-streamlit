4038029afeca11a49dbf796cfc55e7046d88d1f4
ycleungao <ycleungao@connect.ust.hk>
2025-11-17 14:49:18 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -7,5 +7,5 @@ st.header("Manage Input Section")
 st.write("Please enter the monthly sales target and select the region.")
 target = st.number_input("Enter monthly sales target(in USD)",min_value = 0,max_value = 1000000,value = 50000)
 region = st.selectbox("Select Region",["North","East","South","West"])
-st.button("Submit")
-st.success(f"Input successfully! Your monthly sales target is {target} in {region} Region! ")
+if st.button("Submit"):
+  st.success(f"Input successfully! Your monthly sales target is {target} in {region} Region! ")
