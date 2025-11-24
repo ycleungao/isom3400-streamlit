202b82f3e3d3706526a0a24aea48d72e70b7d24f
ycleungao <ycleungao@connect.ust.hk>
2025-11-24 13:59:13 +0800

    Update option_menu.py

--- option_menu.py
+++ option_menu.py
@@ -5,16 +5,18 @@ st.title('Hello, Students!')
 st.write('This is your Python Programming course.')
 
 with st.sidebar:
+    # in the sidebar, we would like to have a option menu
     # customized the option menu
+    # store the entire option menu to a variable so we can have interaction with the users(they can click and change to another page)
     selected=option_menu(
         menu_title = "Menu",
-        options = ["Home", "About", "Contact"],
+        options = ["ISOM3400", "About", "Contact"],
         icons = ["house", "cloud-upload", "list-task"],
-        menu_icon= "emoji-smile-fill",
+        menu_icon= "cast",
         default_index=0,
     )
 
-if selected == "Home":
+if selected == "ISOM3400":
     st.title(f"Welcome to the {selected} page.")
 
 if selected == "About":
